f7a17638dfdc1afb5eadb03b949d0d2c45e6d18f
Ariel Scherman <arielscherman@gmail.com>
2018-05-06 19:21:37 -0300

    fix controller namespace

--- app/controllers/api_events/events_controller.rb
+++ app/controllers/api_events/events_controller.rb
@@ -1,5 +1,8 @@
 module ApiEvents
   class EventsController < ApplicationController
+    # We should add authorization.
+    skip_before_action :verify_authenticity_token
+
     def create
       binding.pry
     end
